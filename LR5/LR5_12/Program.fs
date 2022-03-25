523c45721a1f0038f25bf6062b4c34987ddc595a
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-25 08:18:57 +0300

    Problem 12 is solved

---
+++ LR5/LR5_12/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какой ваш любимый язык программирования?"
+    let choiseL str  = 
+        match str with
+        |"F#"|"Prolog"->"Подлиза"
+        |_->"!Подлиза"
+      
+    //12.1
+    (Console.ReadLine>>choiseL>>Console.WriteLine)()
+    
+    //12.2
+    let f input (output:string->unit) choiseL = output(choiseL(input()))
+    f Console.ReadLine Console.WriteLine choiseL
+    0
