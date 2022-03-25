fa91022db6f4a5cc7b2e9a2b7b9e7891b7468921
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-25 13:39:36 +0300

    Problem 20 is solved

---
+++ LR5/LR5_20/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let ChoiceFunction  y x = 
+    match y with
+    | 1 -> Program.dividersFuncWithPredicate x (fun x -> Program.prime x) (fun x y -> max x y) 0
+    | 2 -> Program.multiplicationDownWithPredicate x (fun x -> x % 5 > 0)
+    | _ -> Program.method3Func x
+
+[<EntryPoint>]
+let main argv =
+
+Console.WriteLine("Введите номер функции и число: ")
+let data = (Console.ReadLine() |> Int32.Parse, Console.ReadLine() |> Int32.Parse)
+let result = ChoiceFunction (fst data) (snd data)
+printfn "Result: %d"  result   
+0
