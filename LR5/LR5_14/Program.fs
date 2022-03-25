0f77f3df5781c9320cb80a4889c908f6cc2cac02
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-25 08:58:24 +0300

    Problem 14 is solved

---
+++ LR5/LR5_14/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let dividersFunc x func init =
+    let rec df x func init curDiv =
+        if curDiv = 0 then init
+        else
+            let newInit= if x % curDiv = 0 then func init curDiv else init
+            let newDiv = curDiv - 1
+            df x func newInit newDiv
+    df x func init x
+        
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Number: ")
+    let x = Console.ReadLine() |> Int32.Parse
+    let result = dividersFunc x (fun x y -> x*y) 1
+    Console.Write("Result: ")
+    Console.WriteLine result
+    0
