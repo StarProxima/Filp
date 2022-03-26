209ef24b9a51debf9723bd5aa8a4c2d03ff8e24e
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-26 11:55:56 +0300

    Problem 14 - 1.21 is solved

---
+++ LR6/LR6_14 - 1.21/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let proc list  =
+    let rec procInternal list curList max =
+        match list with
+        | [] -> curList
+        | elem::t ->
+            let newList = if (elem > max) && t <> [] then curList @ [t.Head] else curList
+            let newMax = if (elem > max) then elem else max
+            procInternal t newList newMax
+    procInternal list [] list.Head
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = Program.readList (Console.ReadLine() |> Convert.ToInt32)
+   
+    Program.printList (proc list)
+    0
