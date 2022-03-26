9d8488c20fde4beb52fe4e92e1fedbcd407df27d
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-26 13:35:25 +0300

    Problem 17 - 1.36 is solved

---
+++ LR6/LR6_17 - 1.36/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let proc list  =
+    let rec procInternal list maxUneven =
+        match list with
+        |[] -> maxUneven
+        | elem0::t ->
+            let newMaxUneven = if (elem0 > maxUneven && elem0 % 2 <> 0) then elem0 else maxUneven
+            procInternal t newMaxUneven
+    procInternal list 0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = Program.readList (Console.ReadLine() |> Convert.ToInt32)
+    
+    let result = proc list
+    match result with
+    | 0 -> printfn "There is no uneven max"
+    |_ -> printf "%d" (proc list)
+    0
