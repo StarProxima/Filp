54beeb70ecadfb562e2ea044ab5cc6b2249a95dc
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-26 12:27:24 +0300

    Problem 16 - 1.33 is solved

---
+++ LR6/LR6_16 - 1.33/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let proc list  =
+    let rec procInternal list isAlternating =
+        match list with
+        |[] -> isAlternating
+        | elem0::elem1::t ->
+            
+            let newIsAlternating = if (elem0*elem1 < 0 && isAlternating) then true else false
+            let newList = elem1::t
+            procInternal newList newIsAlternating
+        |_ -> isAlternating
+    procInternal list true
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = Program.readList (Console.ReadLine() |> Convert.ToInt32)
+   
+    printf "%b" (proc list)
+    0
