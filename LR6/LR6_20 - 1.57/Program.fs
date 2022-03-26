e4e5908fea161f44dd60c49cc368f2c63efce556
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-26 16:03:05 +0300

    Problem 20 - 1.57 is solved

---
+++ LR6/LR6_20 - 1.57/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+let proc list  =
+    let rec procInternal list sum count =
+        match list with
+        |[] -> count
+        | elem0::t ->
+            let newCount = if elem0 > sum then count + 1 else count
+            let newSum = sum + elem0
+            procInternal t newSum newCount
+    procInternal list 0 0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = Program.readList (Console.ReadLine() |> Convert.ToInt32)
+    
+    printf "%d" (proc list)
+    0
