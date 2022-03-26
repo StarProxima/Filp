c6637e48eb47ffce4f4272c8254bb282c6f3eb35
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-26 10:32:12 +0300

    Problem 11 is solved

--- LR6/LR6_11/Program.fs
+++ LR6/LR6_11/Program.fs
@@ -1,4 +1,37 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec printList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        printList tail
+
+let proc3 list func =
+    let rec proc3Internal list func curList =
+        match list with
+        | [] -> curList
+        | elem0::t ->
+            let elem1 = if t <> [] then t.Head else 1
+            let elem2 = if t <> [] then (if t.Tail <> [] then t.Tail.Head else 1) else 1
+            let newElem = func elem0 elem1 elem2
+            let newlist = curList @ [newElem]
+            // Сдвиг списка ещё на 2 вправо
+            let shiftedList = if t <> [] then (if t.Tail <> [] then t.Tail.Tail else []) else []
+            proc3Internal shiftedList func newlist
+    proc3Internal list func []
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+    let newList = proc3 list (fun x y z -> x + y + z)
+    printfn "Result:"
+    printList newList
+    0
