68146ce0516b91affb83bc4e9bfcdbcf30fcdd77
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-26 11:23:57 +0300

    Problem 12 - 1.11 is solved

---
+++ LR6/LR6_12 - 1.11/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+let elemCount list elem =
+    let rec elemCountInternal list elem count  =
+        match list with
+        | [] -> count
+        | elem0::t ->
+            let newCount = if (elem0 = elem) then count + 1 else count
+            elemCountInternal t elem newCount
+    elemCountInternal list elem 0
+
+let proc list =
+    let rec procInternal list originalList elem =
+        match list with
+        | [] -> elem
+        | elem0::t ->
+            let newElem = if (elemCount originalList elem0) = 1 then elem0 else elem
+            procInternal t originalList newElem
+    procInternal list list list.Head
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = Program.readList (Console.ReadLine() |> Convert.ToInt32)
+   
+    printfn "Result: %d" (proc list)
+    0
