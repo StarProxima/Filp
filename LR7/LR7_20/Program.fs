bf9a1088252c5ca2de959f4d4c2e2a87fcd344f9
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 23:09:02 +0300

    Wonderful! Niiiiice teamwork!!!!!
    Problem 20.1 is solved
    
    Co-Authored-By: Екатерина Анищенко <91686883+rmyau@users.noreply.github.com>
    Co-Authored-By: OlegVasil <100850508+OlegVasil@users.noreply.github.com>
    Co-Authored-By: YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
    Co-Authored-By: Валентина Гончаренко <83706757+yellowcat-dotcom@users.noreply.github.com>
    Co-Authored-By: Roman <59415030+r1411@users.noreply.github.com>
    Co-Authored-By: SeemerGG <92618851+SeemerGG@users.noreply.github.com>
    Co-Authored-By: MinAKov <49945726+ColdEiler@users.noreply.github.com>
    Co-Authored-By: AlexanderFeed <96307947+AlexanderFeed@users.noreply.github.com>
    Co-Authored-By: KirSaxhlil <75578152+KirSaxhlil@users.noreply.github.com>
    Co-Authored-By: waasabi13 <96485957+waasabi13@users.noreply.github.com>
    Co-Authored-By: KREEAL <37246218+KREEAL@users.noreply.github.com>
    Co-Authored-By: weeeeweeee <96397307+weeeeweeee@users.noreply.github.com>
    Co-Authored-By: BaevaDiana <96077446+BaevaDiana@users.noreply.github.com>
    Co-Authored-By: iamgirya <84345515+iamgirya@users.noreply.github.com>
    Co-Authored-By: Sultan <73115468+SultanGG505@users.noreply.github.com>
    Co-Authored-By: shurasikk <90204027+shurasikk@users.noreply.github.com>
    Co-Authored-By: disskretkalove <96077142+disskretkalove@users.noreply.github.com>
    Co-Authored-By: Al Maz <94486568+AlexeyMaz@users.noreply.github.com>
    Co-Authored-By: paperchiche <96042644+paperchiche@users.noreply.github.com>
    Co-Authored-By: Jakepps <96076243+Jakepps@users.noreply.github.com>
    Co-Authored-By: Ilya Safonov <72496072+mizmorr@users.noreply.github.com>
    Co-Authored-By: bushmrz <56377675+bushmrz@users.noreply.github.com>
    Co-Authored-By: krissstyu02 <94697684+krissstyu02@users.noreply.github.com>
    Co-Authored-By: Zed Null <47459550+Zed-Null@users.noreply.github.com>
    Co-Authored-By: RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
    Co-Authored-By: GeroLager <41616537+GeroLager@users.noreply.github.com>
    Co-Authored-By: shin8u <98867521+shin8u@users.noreply.github.com>
    Co-Authored-By: ezsx <79762324+ezsx@users.noreply.github.com>
    Co-Authored-By: Daikeri <98869681+Daikeri@users.noreply.github.com>
    Co-Authored-By: Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
    Co-Authored-By: SidorinSergei <95975705+SidorinSergei@users.noreply.github.com>
    Co-Authored-By: [Surface!!u] <60102276+Surface1U@users.noreply.github.com>

--- LR7/LR7_20/Program.fs
+++ LR7/LR7_20/Program.fs
@@ -1,1 +1,56 @@
-﻿
+﻿open System
+
+let rec readListStr n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine()
+        let tail = readListStr (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%s" head
+        writeList tail
+
+
+let proc =
+    let rec procInternal curList candidate ind =
+        if(ind >= 100) then curList
+        else
+            let newCurList = if (candidate % 13 = 0 || candidate % 17 = 0) then curList @ [candidate] else curList
+            let newInd = if (candidate % 13 = 0 || candidate % 17 = 0) then ind + 1 else ind
+            let newCandidate = candidate + 1
+            procInternal newCurList newCandidate newInd
+    procInternal [] 1 0
+
+let countOfTriple (str:string) =
+    let rec countOfTripleInternal index count =
+        match index with
+        | index when index >= str.Length - 2 -> count
+        |_ ->
+            let newCount = if str.[index] = str.[index+2] then count + 1 else count
+            let newIndex = index + 1
+            countOfTripleInternal newIndex newCount
+    countOfTripleInternal 0 0
+
+let sortByTriple list =
+    List.sortBy (fun x -> -(countOfTriple x)) list
+            
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and strings:"
+    let list = readListStr (Console.ReadLine() |> Convert.ToInt32)
+    printfn "Number of sort: 1 or 2: "
+    let number = 2
+    match number with
+    | 1 ->
+        printfn "Sort1: "
+        printfn "No Result:"
+        
+    | 2 -> 
+        printfn "Sort2: "
+        printfn "Result: %A" (sortByTriple list)
+    | _ ->
+        printfn "No comments: "
+    0
