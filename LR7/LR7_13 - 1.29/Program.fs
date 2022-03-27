201654ae92111e9e343d5315061cae1080c34e3a
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 15:18:09 +0300

    Problem 13 - 1.29 is solved

--- LR7/LR7_13 - 1.29/Program.fs
+++ LR7/LR7_13 - 1.29/Program.fs
@@ -1,4 +1,29 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+let proc list a b =
+    if List.max list > a && List.max list < b then true else false
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+    printfn "a and b:"
+    let a = (Console.ReadLine() |> Convert.ToInt32)
+    let b = (Console.ReadLine() |> Convert.ToInt32)
+    printfn "Result"
+    printf "%b" (proc list a b)
+    0
