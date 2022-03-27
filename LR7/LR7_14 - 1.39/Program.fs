1f6a2b1dce64e197e77ae82fc7271c9fa98bbc29
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 15:27:45 +0300

    Problem 14 - 1.39 is solved

--- LR7/LR7_14 - 1.39/Program.fs
+++ LR7/LR7_14 - 1.39/Program.fs
@@ -1,4 +1,28 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+let writeEvenUneven list =
+    List.iteri (fun i x -> if i % 2 = 0 then (printfn "%d" x)) list
+    List.iteri (fun i x -> if i % 2 <> 0 then (printfn "%d" x)) list
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    printfn "Result"
+    writeEvenUneven list
+    0
