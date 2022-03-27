5686658d3f51a4ed270306fa86d7d60572791fe9
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 19:12:47 +0300

    Problem 17 is solved
    stuffy mode

--- LR7/LR7_17 - 9/Program.fs
+++ LR7/LR7_17 - 9/Program.fs
@@ -1,4 +1,43 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+let sumBeforeIndex list index = 
+    List.fold (fun acc elem -> acc + elem) 0 (List.init index (fun idx -> List.item idx list)) 
+
+let isFullSquare list x =
+    (List.filter (fun a -> a*a = x) list).Length > 1
+
+let isDividedIntoAllElemBefore list index =
+    (List.filter (fun a -> (List.item index list) % a = 0) (List.init index (fun idx -> List.item idx list)) ).Length = index 
+    
+
+let proc list =
+    let rec procInternal nowList curList ind =
+        match nowList with
+        | [] -> curList
+        | elem::t ->
+            let newCurList = if (elem > sumBeforeIndex list ind && isFullSquare list elem && isDividedIntoAllElemBefore list ind) then curList @ [elem] else curList
+            let newInd = ind + 1
+            procInternal t newCurList newInd
+    procInternal list [] 0
+            
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    printfn "Result"
+    writeList (proc list)
+    0
