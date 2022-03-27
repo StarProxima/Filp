cffc22537c3760c6fd81043c58d7f1fe1d5663ed
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 19:30:17 +0300

    Problem 18 is solved

--- LR7/LR7_18 - 9/Program.fs
+++ LR7/LR7_18 - 9/Program.fs
@@ -1,4 +1,34 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+
+let proc =
+    let rec procInternal curList candidate ind =
+        if(ind >= 100) then curList
+        else
+            let newCurList = if (candidate % 13 = 0 || candidate % 17 = 0) then curList @ [candidate] else curList
+            let newInd = if (candidate % 13 = 0 || candidate % 17 = 0) then ind + 1 else ind
+            let newCandidate = candidate + 1
+            procInternal newCurList newCandidate newInd
+    procInternal [] 1 0
+            
+[<EntryPoint>]
+let main argv =
+    //printfn "Number of items and list:"
+    //let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    printfn "Result"
+    writeList (proc)
+    0
