b22c2d1ee10ce072e03dd65f8cff770513554f48
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 16:23:57 +0300

    Problem 15 - 1.49 is solved

--- LR7/LR7_15 - 1.49/Program.fs
+++ LR7/LR7_15 - 1.49/Program.fs
@@ -1,4 +1,45 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+let isPrime x =
+    let rec isPrimeInternal divCandidate =
+        if divCandidate >= x then true
+        else
+            if (x % divCandidate = 0) then false
+            else 
+                let newDivCandidate = divCandidate + 1
+                isPrimeInternal newDivCandidate
+    isPrimeInternal 2
+
+let dividersFunc x =
+    let rec dividersFuncInternal curList divCandidate =
+        if divCandidate > x then curList
+        else
+            let newCurList = if (x % divCandidate = 0 && isPrime divCandidate) then curList @ [divCandidate] else curList
+            let newDivCandidate = divCandidate + 1
+            dividersFuncInternal newCurList newDivCandidate
+    dividersFuncInternal [] 1
+        
+let proc list =
+    List.distinct ( List.fold  (fun acc x -> List.append acc (dividersFunc x)) [] list )
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+    
+    printfn "Result"
+    writeList (proc list)
+    0
