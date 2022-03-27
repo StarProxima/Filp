f6d4eb09020dcc152915624f2bcdb46e9599993f
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 16:50:33 +0300

    Problem 16 - 1.59 is solved

--- LR7/LR7_16 - 1.59/Program.fs
+++ LR7/LR7_16 - 1.59/Program.fs
@@ -1,4 +1,31 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+let proc list =
+    List.choose (fun x ->
+    match x with
+    | x when x < 100 && x>=0 && (List.filter(fun a -> a=x) list).Length > 2 -> Some(x*x)
+    | _ -> None
+    ) (List.distinct list)
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    printfn "Result"
+    writeList (proc list)
+    0
