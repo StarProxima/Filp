70c51997b7f9bcb4b66cff314d260003779cc65d
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 20:39:24 +0300

    Problem 19.1 is solved

--- LR7/LR7_19/Program.fs
+++ LR7/LR7_19/Program.fs
@@ -1,4 +1,29 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let task3 (str:string) =
+    let splitted = str.Split " "
+    let rnd = System.Random()
+    let res = Array.sortBy (fun x -> rnd.Next(0, 100)) splitted
+    String.concat " " res
+
+
+let createStrArray n =
+    let rec createStrArrayInternal tn strArray =
+        if(tn >= n) then strArray
+        else
+            let str = Console.ReadLine()
+            let newN = tn+1
+            let newStrArray = Array.append strArray [|str|]
+            createStrArrayInternal newN newStrArray
+    createStrArrayInternal 0 [||]
+    
+         
+[<EntryPoint>]
+let main argv =
+    printfn "String: "
+
+    let str =Console.ReadLine()
+    printfn "Result: "
+    printfn "%A" (task3 str)
+    
+    0
