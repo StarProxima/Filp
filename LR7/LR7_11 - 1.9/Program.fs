0da237ac93b9bdb3c4fa278e7e0f0027157c39c3
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 14:53:25 +0300

    Problem 11 - 1.9 is solved, add projects

--- LR7/LR7_11 - 1.9/Program.fs
+++ LR7/LR7_11 - 1.9/Program.fs
@@ -1,4 +1,27 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+let trimListByLastMin list =
+    List.init (List.findIndexBack (fun x -> x = List.min list) list) (fun index -> List.item index list)
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    printfn "Result"
+    writeList (trimListByLastMin list)
+    0
