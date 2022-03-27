64e6163d40f2517e1f0fb7a0de1a87a6b3e4ef60
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-27 15:10:19 +0300

    Problem 12 - 1.19 is solved

--- LR7/LR7_12 - 1.19/Program.fs
+++ LR7/LR7_12 - 1.19/Program.fs
@@ -1,4 +1,29 @@
-﻿
+﻿open System
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let rec readList n = 
+    if n=0 then []
+    else
+        let head = Console.ReadLine() |> Convert.ToInt32
+        let tail = readList (n-1)
+        head::tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        printfn "%O" head
+        writeList tail
+
+let shiftRight list n =
+    let len = List.length list
+    let newIndex idx = if idx - n < 0 then len + (idx - n) else idx - n 
+    List.init len (fun index -> List.item (newIndex index) list)
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "Number of items and list:"
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    printfn "Result"
+    writeList (shiftRight list 1)
+    0
