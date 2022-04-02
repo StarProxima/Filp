2ac9d78b20b2e2d6a2e477cbe9dfed27d6984a65
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-02 09:05:30 +0300

    Problem 20.2 is solved

--- LR7/LR7_20/Program.fs
+++ LR7/LR7_20/Program.fs
@@ -14,15 +14,15 @@ let rec writeList = function
         writeList tail
 
 
-let proc =
-    let rec procInternal curList candidate ind =
-        if(ind >= 100) then curList
-        else
-            let newCurList = if (candidate % 13 = 0 || candidate % 17 = 0) then curList @ [candidate] else curList
-            let newInd = if (candidate % 13 = 0 || candidate % 17 = 0) then ind + 1 else ind
-            let newCandidate = candidate + 1
-            procInternal newCurList newCandidate newInd
-    procInternal [] 1 0
+//let proc =
+//    let rec procInternal curList candidate ind =
+//        if(ind >= 100) then curList
+//        else
+//            let newCurList = if (candidate % 13 = 0 || candidate % 17 = 0) then curList @ [candidate] else curList
+//            let newInd = if (candidate % 13 = 0 || candidate % 17 = 0) then ind + 1 else ind
+//            let newCandidate = candidate + 1
+//            procInternal newCurList newCandidate newInd
+//    procInternal [] 1 0
 
 let countOfTriple (str:string) =
     let rec countOfTripleInternal index count =
@@ -35,18 +35,42 @@ let countOfTriple (str:string) =
     countOfTripleInternal 0 0
 
 let sortByTriple list =
-    List.sortBy (fun x -> -(countOfTriple x)) list
+    List.sortBy (fun x -> (countOfTriple x)) list
+
+let maxASCII (str:string) =
+    let rec maxASCIIInternal index (count:int) =
+        match index with
+        | index when index >= str.Length - 1 -> count
+        |_ ->
+            let newCount = if (str.[index] |> Convert.ToInt32)  > count then (str.[index] |> Convert.ToInt32) else count
+            let newIndex = index + 1
+            maxASCIIInternal newIndex newCount
+    maxASCIIInternal 0 ('A' |> Convert.ToInt32)
+    
+let sumRazn (str:string) =
+    let rec task9Internal index index2 count =
+        match index with
+        | index when index2 <= index -> count
+        |_ ->
+            let newCount = (str.[index] |> Convert.ToInt32) - (str.[index2] |> Convert.ToInt32) + count
+            let newIndex = index + 1
+            let newIndex2 = index2 - 1
+            task9Internal newIndex  newIndex2 newCount
+    task9Internal 0 ((str.Length) - 1) 0
+
+let task9 list =
+    List.sortBy (fun x -> (maxASCII x - sumRazn x)*(maxASCII x - sumRazn x) ) list
             
 [<EntryPoint>]
 let main argv =
     printfn "Number of items and strings:"
     let list = readListStr (Console.ReadLine() |> Convert.ToInt32)
     printfn "Number of sort: 1 or 2: "
-    let number = 2
+    let number = 1
     match number with
     | 1 ->
         printfn "Sort1: "
-        printfn "No Result:"
+        printfn "Result: %A" (task9 list)
         
     | 2 -> 
         printfn "Sort2: "
