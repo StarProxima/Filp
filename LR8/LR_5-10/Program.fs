ce151fc42ba778cd1d819550fd4f8d85e188ab11
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-14 14:15:22 +0300

    Problem 6 and rename

---
+++ LR8/LR_5-10/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+type DriversLicense(
+    firstName: string,
+    lastName: string,
+    series: int,
+    number: int,
+    expirationDate: DateTime) =
+
+    member this.firstName = firstName
+    member this.lastName = lastName
+    member this.series = series
+    member this.number = number
+    member this.expirationDate = expirationDate
+
+    override this.ToString() = $"Водительские права:
+    Имя: {this.firstName}
+    Фамилия: {this.lastName} 
+    Серия: {this.series} 
+    Номер: {this.number} 
+    Дата истечения: {this.expirationDate.ToShortDateString()}"
+
+
+
+[<EntryPoint>]
+let main argv =
+    let licence = DriversLicense("Гиряндр", "Данилков", 1314, 678634, DateTime.Parse "16.04.2022")
+    Console.WriteLine(licence)
+    0
