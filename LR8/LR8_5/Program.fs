3ac93b14a09e3167edc53f14f076f714de775a1a
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-14 14:06:14 +0300

    add Project and Problem 5

---
+++ LR8/LR8_5/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+type DriversLicense(
+    firstName: string,
+    lastName: string,
+    series: int,
+    number: int,
+    expirationDate: DateTime) =
+
+    member this.firstName = firstName
+    member this.lastName = lastName
+    member this.series = series
+    member this.number = number
+    member this.expirationDate = expirationDate
+
+
+[<EntryPoint>]
+let main argv =
+    0
