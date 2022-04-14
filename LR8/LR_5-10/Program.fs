e1aa2a9b39c16db7f437cff4e6e7d01828ad9819
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-14 14:29:00 +0300

    Problem 7

--- LR8/LR_5-10/Program.fs
+++ LR8/LR_5-10/Program.fs
@@ -20,10 +20,29 @@ type DriversLicense(
     Номер: {this.number} 
     Дата истечения: {this.expirationDate.ToShortDateString()}"
 
+    interface IComparable with
+        member this.CompareTo(obj: obj): int = 
+            match obj with
+            | :? DriversLicense as licence -> if this.series = licence.series then this.number.CompareTo licence.number else this.series.CompareTo licence.series
+            | _ -> invalidArg "obj" "Разные типы сравниваемых объектов" 
+
+    override this.Equals(obj) =
+        match obj with
+        | :? DriversLicense as licence -> this.series = licence.series && this.number = licence.number
+        | _ -> invalidArg "obj" "Разные типы сравниваемых объектов"
+
+    override this.GetHashCode() =
+        HashCode.Combine(series, number)
+
+    
+
+
 
 
 [<EntryPoint>]
 let main argv =
-    let licence = DriversLicense("Гиряндр", "Данилков", 1314, 678634, DateTime.Parse "16.04.2022")
-    Console.WriteLine(licence)
+    let licence1 = DriversLicense("Гиряндр", "Данилков", 1314, 678634, DateTime.Parse "16.04.2022")
+    let licence2 = DriversLicense("НеГиряндр", "НеДанилков", 1314, 678635, DateTime.Parse "16.04.2022")
+    Console.WriteLine(licence1)
+    Console.WriteLine(licence1 <> licence2)
     0
