8f7b37a9a499ab9119dfa307b0dfcecbfba2e59c
StarProxima <maks.prozorov1@yandex.ru>
2022-04-30 01:19:30 +0300

    AI Done
    Problem 4

---
+++ LR8/LR8_4/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let agent = MailboxProcessor.Start(fun inbox->
+    let rec messageLoop() = async{
+        let! msg = inbox.Receive()
+        
+        let str = 
+            match msg with
+            |"F#"|"Prolog"->"Подлиза"
+            |other -> $"Чел ты, щас бы в 3022 юзать '{other}'"
+
+        printfn "AI: %s" str
+        return! messageLoop()
+        }
+    messageLoop()
+    )
+
+let rec dialogue() =
+    let input = Console.ReadLine().Trim()
+    if input<>"" then
+        agent.Post(input)
+        dialogue()
+
+[<EntryPoint>]
+let main argv =
+    printfn "Start a dialogue with artificial intelligence. Enter for closing the dialogue."
+    dialogue()
+    printfn "Thansk for using. Developed by OpenAI in 3022."
+    0 
