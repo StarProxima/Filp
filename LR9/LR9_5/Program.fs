12f98314845b14cdc1d43ff1c58d8f5a9c2eecdc
StarProxima <maks.prozorov1@yandex.ru>
2022-05-01 22:20:57 +0300

    Problem 5

--- LR9/LR9_5/Program.fs
+++ LR9/LR9_5/Program.fs
@@ -1,1 +1,17 @@
-﻿
+﻿open System.Windows.Forms
+
+let Form = new Form(Width = 600, Height = 100, Text = "LR9_5")
+
+let ProgressBar1 = new ProgressBar(Dock = DockStyle.Bottom)
+let TextBox1 = new TextBox (Width = 300, Height=100, Top=20, Left = 150, MaxLength = 50)
+
+let changes _ = ProgressBar1.Value <- TextBox1.TextLength*2
+TextBox1.TextChanged.Add(changes)
+
+Form.Controls.Add(TextBox1)
+Form.Controls.Add(ProgressBar1)
+
+[<EntryPoint>]
+let main argv =
+    do Application.Run(Form)
+    0
