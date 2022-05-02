ab42cf552114d811572094bd0433012f6ebd4297
StarProxima <maks.prozorov1@yandex.ru>
2022-05-03 01:55:20 +0300

    Problem 1.2

--- PrologLabs/LR12.pl
+++ PrologLabs/LR12.pl
@@ -1,6 +1,4 @@
-
-
-% 11
+% 11.1
 prime_internal(X,X).
 prime_internal(1,_).
 prime_internal(Number, Iter) :- 
@@ -9,11 +7,24 @@ prime_internal(Number, Iter) :-
     prime_internal(Number, NewIter),!.
 prime(X) :- prime_internal(X,2).
 
-max_prime_pivider_down(X,Y,Del) :-
+max_prime_divider_down(X,Y,Del) :-
     0 is X mod Del,
     prime(Del),
     Y is Del,!.
-max_prime_pivider_down(X,Y,Del) :-
+max_prime_divider_down(X,Y,Del) :-
     NewDel is Del - 1,
-    max_prime_pivider_down(X,Y,NewDel).
-max_prime_divider(X,Y) :- max_prime_pivider_down(X,Y,X).
+    max_prime_divider_down(X,Y,NewDel).
+max_prime_divider_down(X,Y) :- max_prime_divider_down(X,Y,X).
+
+% 11.2
+% Или вместо этого просто Y is max(C, NewDel)
+max(Y, C, NewDel) :- NewDel > C, Y is NewDel,!.
+max(Y, C, NewDel) :- NewDel < C, Y is C,!.
+max(Y, C, NewDel) :- Y is C.
+
+max_prime_divider_up(_,Y,Del) :- Del < 2, Y is 1.
+max_prime_divider_up(X,Y,Del) :-
+    NewDel is Del - 1,
+    (0 is X mod NewDel, prime(NewDel), max_prime_divider_up(X,C,NewDel), max(Y, C, NewDel),! ;  max_prime_divider_up(X,Y,NewDel)).
+max_prime_divider_up(X,Y) :- max_prime_divider_up(X,Y,X+1).
+
