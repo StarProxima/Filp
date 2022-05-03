9159a8eacd91604e1dfc9c1396952759337c8dd9
StarProxima <maks.prozorov1@yandex.ru>
2022-05-04 01:20:30 +0300

    Problem 2 - 45

--- PrologLabs/LR13.pl
+++ PrologLabs/LR13.pl
@@ -1,3 +1,5 @@
+
+
 % 11 - 39
 
 print_even_uneven_index_list([Elem|Tail]) :- print_even_uneven_index_list([Elem|Tail], [Elem|Tail], 1),!.
@@ -11,3 +13,15 @@ print_even_uneven_index_list(List, [Elem|Tail], Index) :-
     print_even_uneven_index_list(List, Tail, NewIndex);
     NewIndex is Index+1,
     print_even_uneven_index_list(List, Tail, NewIndex)).
+
+
+% 12 - 45
+
+inInteval(Elem, A,B, X):- Elem > A, Elem < B, X is Elem.
+inInteval(_, _,_, 0).
+
+sum_interval([], _, _, 0).
+sum_interval([Elem|Tail], A, B, Ans) :- 
+    inInteval(Elem, A, B, Inc),
+    sum_interval(Tail, A, B, Ans1),
+    Ans is Ans1 + Inc,!.
