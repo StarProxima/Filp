c69f04f4c1ec787181138c98eca79a12a532e41f
StarProxima <maks.prozorov1@yandex.ru>
2022-05-04 02:10:46 +0300

    Problem 4
    Жестб, это же пролог, про него ещё Жук рассказывал :alien:

--- PrologLabs/LR13.pl
+++ PrologLabs/LR13.pl
@@ -39,3 +39,22 @@ count_elem([Elem|Tail], Sum, Ans):-
     NewSum is Sum+Elem,
     count_elem(Tail, NewSum, Ans1),
     Ans is Ans1 + Inc.
+
+% 14
+
+add_find_list([Elem|_], Elem).
+add_find_list([_|Tail], Elem) :- add_find_list(Tail, Elem).
+
+get_answer14:- get_answer14([_,_,_]).
+get_answer14(List) :- 
+    add_find_list(List,[belocurov,_]),
+    add_find_list(List,[rizov,_]),
+    add_find_list(List,[chernov,_]),
+    add_find_list(List,[_,blondin]),
+    add_find_list(List,[_,riziy]),
+    add_find_list(List,[_,brunet]), write(0),
+    not(add_find_list(List,[belocurov,brunet])), write(1),
+    not(add_find_list(List,[belocurov,blondin])), write(2),
+    not(add_find_list(List,[rizov,riziy])), write(3), 
+    not(add_find_list(List,[chernov,brunet])), nl,
+    write(List).
