aa29ff4b7563cb26b634f8eb87452bb9bf30c056
StarProxima <maks.prozorov1@yandex.ru>
2022-05-11 01:38:54 +0300

    Problem 1.3

--- PrologLabs/LR14.pl
+++ PrologLabs/LR14.pl
@@ -44,3 +44,54 @@ wordsCount([H|T], Count):-
 		Count is N+1;
 		wordsCount(T,Count).
 
+% 1.3 Дана строка, определить самое частое слово
+
+task1_3 :- readString(Str, _), mostCommonWord(Str, X), writeString(X).
+
+
+
+count(List, X, Ans) :- 
+    count(List, X, 0, Ans).
+count([], _, Ans, Ans) :- !.
+count([X|T], X, CurCnt, Ans) :- 
+    NewCnt is CurCnt + 1, 
+    count(T, X, NewCnt, Ans),!.
+count([_|T], X, CurCnt, Ans) :- 
+    count(T, X, CurCnt, Ans),!.
+
+
+
+splitString([], _, CurWord, CurWordList, Ans) :- 
+    appendString(CurWordList, [CurWord], NewWL), 
+    Ans = NewWL,!.
+
+splitString([Separator|T], Separator, CurWord, CurWordList, Ans) :- 
+    appendString(CurWordList, [CurWord], NewWL), 
+    splitString(T, Separator, [], NewWL, Ans),!.
+
+splitString([S|T], Separator, CurWord, CurWordList, Ans) :- 
+    appendString(CurWord, [S], NewWord), 
+    splitString(T, Separator, NewWord, CurWordList, Ans),!.
+
+splitString(Str, Separator, Ans) :- 
+    char_code(Separator, SepCode), 
+    splitString(Str, SepCode, [], [], Ans).
+
+
+
+mostCommonWord(Str, Ans) :-
+    splitString(Str, " ", Words), 
+    mostCommonWord(Words, Words, 0, [], Ans). 
+
+mostCommonWord(Words, [Word|T], CurMaxCnt, _, Ans) :- 
+    count(Words, Word, Cnt), 
+    Cnt > CurMaxCnt, 
+    NewMax is Cnt, 
+    NewMaxWord = Word, 
+    mostCommonWord(Words, T, NewMax, NewMaxWord, Ans),!.
+
+mostCommonWord(Words, [_|T], CurMaxCnt, CurMaxWord, Ans) :- 
+    mostCommonWord(Words, T, CurMaxCnt, CurMaxWord, Ans),!.
+
+mostCommonWord(_, [], _, Ans, Ans) :-!.
+
